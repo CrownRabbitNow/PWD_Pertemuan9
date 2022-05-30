431becf72623832529505d8670f5644d2697a650
firman <rizkyfirman357@gmail.com>
2022-05-30 09:26:22 +0700

    Mencoba untuk commit dan push

---
+++ Latihan.php
@@ -0,0 +1,23 @@
+<?php 
+    //menampilkan data ke layar
+// echo"Hello World";
+$nama = "Kiki";
+var_dump($nama);
+echo $nama;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="UTF-8" />
+   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+   <title>Latihan PHP</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
